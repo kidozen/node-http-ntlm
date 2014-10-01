ec9f748d19a7e32acd884ae54b600742c7468282
Hernán Tierno <htierno@gmail.com>
2014-10-01 18:27:55 -0300

    Cherry pick  to add additional headers

--- httpntlm.js
+++ httpntlm.js
@@ -3,56 +3,62 @@ var url = require('url');
 var httpreq = require('httpreq');
 var ntlm = require('./ntlm');
 
-exports.method = function(method, options, callback){
-	if(!options.workstation) options.workstation = '';
-	if(!options.domain) options.domain = '';
+exports.method = function (method, options, callback) {
+    "use strict";
+    if(!options.workstation) options.workstation = '';
+    if(!options.domain) options.domain = '';
 
-	// is https?
-	var isHttps = false;
-	var reqUrl = url.parse(options.url);
-	if(reqUrl.protocol == 'https:') isHttps = true;
+    // is https?
+    var isHttps = false;
+    var reqUrl = url.parse(options.url);
+    if(reqUrl.protocol === 'https:') isHttps = true;
 
-	// set keepaliveAgent (http or https):
-	var keepaliveAgent;
+    // set keepaliveAgent (http or https):
+    var keepaliveAgent;
 
-	if(isHttps){
-		var HttpsAgent = require('agentkeepalive').HttpsAgent;
-		keepaliveAgent = new HttpsAgent();
-	}else{
-		var Agent = require('agentkeepalive');
-		keepaliveAgent = new Agent();
-	}
+    if (isHttps) {
+        var HttpsAgent = require('agentkeepalive').HttpsAgent;
+        keepaliveAgent = new HttpsAgent();
+    } else {
+        var Agent = require('agentkeepalive');
+        keepaliveAgent = new Agent();
+    }
 
-	async.waterfall([
-		function ($){
-			var type1msg = ntlm.createType1Message(options);
+    exports.httpAgent = keepaliveAgent;
+    exports.httpClient = httpreq;
 
-			httpreq.get(options.url, {
-				headers:{
-					'Connection' : 'keep-alive',
-					'Authorization': type1msg
-				},
-				agent: keepaliveAgent
-			}, $);
-		},
+    async.waterfall([
+        function ($) {
+            var type1msg = ntlm.createType1Message(options);
 
-		function (res, $){
-			if(!res.headers['www-authenticate'])
-				return $(new Error('www-authenticate not found on response of second request'));
+            httpreq.get(options.url, {
+                headers:{
+                    'Connection' : 'keep-alive',
+                    'Authorization': type1msg
+                },
+                agent: keepaliveAgent
+            }, $);
+        },
 
-			var type2msg = ntlm.parseType2Message(res.headers['www-authenticate']);
-			var type3msg = ntlm.createType3Message(type2msg, options);
+        function (res, $){
+            if(!res.headers['www-authenticate'])
+                return $(new Error('www-authenticate not found on response of second request'));
 
-			httpreq[method](options.url, {
-				headers:{
-					'Connection' : 'Close',
-					'Authorization': type3msg
-				},
-				allowRedirects: false,
-				agent: keepaliveAgent
-			}, $);
-		}
-	], callback);
+            var type2msg = ntlm.parseType2Message(res.headers['www-authenticate']);
+            var type3msg = ntlm.createType3Message(type2msg, options);
+
+            var headers = options.headers || {};
+            headers.Connection = "keep-alive";
+            headers.Authorization = type3msg;
+
+            httpreq[method](options.url, {
+                headers: headers,
+                allowRedirects: false,
+                agent: keepaliveAgent
+            }, $);
+        }
+
+    ], callback);
 };
 
 ['get', 'put', 'post', 'delete', 'head'].forEach(function(method){
